4121b5c309fc52cd9633bbebe7ab7fa7232cc0fa
Eng-And <94868850+Eng-And@users.noreply.github.com>
2023-01-28 12:23:04 -0500

    Grabbed New Dev Stuff :)

--- Charge2023/src/main/java/frc/robot/subsystems/Vision.java
+++ Charge2023/src/main/java/frc/robot/subsystems/Vision.java
@@ -4,11 +4,38 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
+
+  private final NetworkTable m_limelightTable;
+
+  private double m_xOffset; // horizontal offset from crosshair to target (-27 degrees to 27 degrees)
+  private double m_yOffset; // vertical offset from crosshair to target (-20.5 degrees to 20.5 degrees)
+
   /** Creates a new Vision. */
-  public Vision() {}
+  public Vision() {
+
+    m_limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+
+  }
+
+  // Returns the horizontal offset to the target
+  public double getXOffset() {
+    return m_xOffset;
+  }
+
+  // Returns the vertical offset to the target
+  public double getYOffset() {
+    return m_yOffset;
+  }
+
+  public void updateLimelight() {
+    m_xOffset = m_limelightTable.getEntry("tx").getDouble(0.0); // Key and defaultvalue cannot be removed, vscode doesn't allow it -\( '-' )/-
+    m_yOffset = m_limelightTable.getEntry("ty").getDouble(0.0);
+  }
 
   @Override
   public void periodic() {
