d1609e38e97ff0cc6976c0ec43c4ebd5997399f2
Maya Z <57159221+657323@users.noreply.github.com>
2023-02-01 16:09:46 -0500

    Revert "Delete Charge2023Test directory"
    This reverts commit 25ae63994fa4d0e211e120b60a0badb72f95197e.

---
+++ Charge2023Test/src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,100 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
+  
+  private final NetworkTable m_limelightTable;
+
+  private final NetworkTableEntry m_targetDistance;
+  private final NetworkTableEntry m_targetDetected;
+
+  private double m_xOffset; // horizontal offset from crosshair to target (-27 degrees to 27 degrees)
+  private double m_yOffset; // vertical offset from crosshair to target (-20.5 degrees to 20.5 degrees)
+  private double m_percentArea; // target area
+  private double m_targetValue; // whether the limelight has an valid targets (0 or 1)
+
+  private double[] m_cameraPose;
+  private double[] m_targetPose;
+
+  private double[] defaultDoubleArray;
+
+  
+  /** Creates a new Vision. */
+  public Vision() {
+
+    // Instantiate the network table
+    m_limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+
+    // Initialize the network table entries for target distance and target detected
+    m_targetDistance = m_limelightTable.getEntry("Target Distance");
+    m_targetDetected = m_limelightTable.getEntry("Target Detected");
+
+    defaultDoubleArray = new double[6];
+    
+    turnOnLED();
+  }
+
+  // Returns the horizontal offset to the target 
+  public double getXOffset(){
+    return m_xOffset;
+  }
+
+  // Returns the vertical offset to the target 
+  public double getYOffset(){
+    return m_yOffset;
+  }
+
+  public double getpercentArea(){
+    return m_percentArea;
+  }
+
+  public double getTargetValue(){
+    return m_targetValue;
+  }
+
+  public double[] getCameraPose(){
+    return m_cameraPose;
+  }
+
+  public double[] getTargetPose(){
+    return m_targetPose;
+  }
+  
+  public boolean isTargetDetected(){
+    return (m_targetValue > 0.0);
+  }
+
+  public void turnOnLED(){
+    m_limelightTable.getEntry("LED Mode").setNumber(3);
+  }
+
+  public void turnOffLED(){
+    m_limelightTable.getEntry("LED Mode").setNumber(1);
+  }
+
+  public void updateLimeLight(){
+    m_xOffset = m_limelightTable.getEntry("tx").getDouble(0.0);
+    m_yOffset = m_limelightTable.getEntry("ty").getDouble(0.0);
+    m_percentArea = m_limelightTable.getEntry("ta").getDouble(0.0);
+    m_targetValue = m_limelightTable.getEntry("tv").getDouble(0.0);
+
+    m_cameraPose = m_limelightTable.getEntry("t6c_ts").getDoubleArray(defaultDoubleArray);
+    m_targetPose = m_limelightTable.getEntry("t6t_cs").getDoubleArray(defaultDoubleArray);
+
+    m_targetDetected.setBoolean(isTargetDetected());
+  }
+
+ 
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
