de7501eeaf990508432af13d7bfaf533cdd14a7e
Smiles360 <jakeleff18@gmail.com>
2023-02-01 16:12:47 -0500

    Finished Arm Subsystem

--- Charge2023Test/src/main/java/frc/robot/Constants.java
+++ Charge2023Test/src/main/java/frc/robot/Constants.java
@@ -246,5 +246,34 @@ public final class Constants {
         public static final int kOperatorControllerPort = 1;
     }
 
+    public static final class ArmConstants {
+        // need all port nums
+        public static final int kExtensionMotorPort = -1;
+        public static final int kAngleMotorPort = -1;
+        public static final int kStringPotPort = -1;
+        public static final double kMaxExtensionLength = 63.75; // inches
+        // tbd
+        public static final double kAngleMotorSpeed = .5;
+        public static final double kExtensionMotorSpeed = .5;
+        // grid constants
+        public static final double leftHorizontalShift = 0;
+        public static final double midHorizontalShift = 0;
+        public static final double rightHorizontalShift = 0;
+        public static final double lowAngle = 0;
+        public static final double midConeAngle = 0;
+        public static final double highConeAngle = 0;
+        public static final double midCubeAngle = 0;
+        public static final double highCubeAngle = 0;
+        public static final double lowExtensionDistance = 0;
+        public static final double midExtensionDistance = 0;
+        public static final double highExtensionDistance = 0;
+        // shelf constants
+        public static final double shelfAngle = 0;
+        public static final double shelfExtensionDistance = 0;
+        // charge station constants
+        public static final double chargingAngle = 0;
+        public static final double chargingExtensionDistance = 0;
+    }
+
 
 }
---
+++ Charge2023Test/src/main/java/frc/robot/commandgroups/GridMidLeft.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commandgroups;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.ChangeArmAngle;
+import frc.robot.commands.PointTurnGyroTank;
+import frc.robot.commands.TelescopeToDistance;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Drivetrain;
+import static frc.robot.Constants.ArmConstants.*;
+import static frc.robot.Constants.DriveConstants.kTurnSpeed;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+
+// This command group positions the arm to the mid level of the grid in the left position.
+
+public class GridMidLeft extends ParallelCommandGroup {
+
+  private final Arm m_arm;
+  private final Drivetrain m_drivetrain;
+
+  /** Creates a new gridHighLeft. */
+  public GridMidLeft(Arm arm, Drivetrain drivetrain) {
+
+    m_arm = arm;
+    m_drivetrain = drivetrain;
+
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(new PointTurnGyroTank(kTurnSpeed, leftHorizontalShift, m_drivetrain), new ChangeArmAngle(m_arm, midConeAngle), new TelescopeToDistance(m_arm, midExtensionDistance)); 
+  }
+}
