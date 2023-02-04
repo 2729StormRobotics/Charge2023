6f40a787cfb7277bb5802b36036ac6d782184c32
STORM Robotics Driver Station <94648535+sierraciabattoni@users.noreply.github.com>
2023-02-04 11:16:57 -0500

    ChargeStation-Working

--- Charge2023Test/src/main/java/frc/robot/commands/PointTurnGyroTank.java
+++ Charge2023Test/src/main/java/frc/robot/commands/PointTurnGyroTank.java
@@ -33,6 +33,7 @@ public class PointTurnGyroTank extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+     m_drivetrain.calibrategyro();
      m_drivetrain.resetGyro();
 
   }
@@ -53,7 +54,8 @@ public class PointTurnGyroTank extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    
+    m_drivetrain.resetGyro();
+
     return Math.abs(m_drivetrain.getRobotAngle()) >= Math.abs(m_angle);
   }
 }
